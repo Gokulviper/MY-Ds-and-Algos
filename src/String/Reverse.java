e2c360e28972bd1acd4cf9b4e780aade3b39f951
GOKUL <88918972+Gokulviper@users.noreply.github.com>
2022-02-04 15:33:37 +0530

    Update Reverse.java

--- src/String/Reverse.java
+++ src/String/Reverse.java
@@ -5,73 +5,8 @@ import java.util.Stack;
 
 public class Reverse {
     public static void main(String[] args) {
-        System.out.println(validPalindrome("    baa"));
+      
 
     }
-    public static String reverseWords(String s) {
-        Stack<String> stack=new Stack();
-        String temp="",ans="";
-        int i=0;
-        while (i<s.length()&&s.charAt(i)==' ')i++;
-
-       for(;i<s.length();i++){
-
-           if(s.charAt(i)==' '){
-
-                    if(!temp.isBlank())
-               stack.push(temp);
-               temp="";
-
-           }
-           if(s.charAt(i)!=' ')
-           temp+=s.charAt(i);
-       }
-      if(!temp.isBlank()) stack.push(temp);
-       while (!stack.isEmpty()){
-           if(stack.size()>1) {
-               ans = ans + stack.pop() + " ";
-           }
-           if(stack.size()==1) ans = ans + stack.pop();
-       }
-       return ans;
-    }
-    public static String reverseString(String s){
-        char[] ch=s.toCharArray();
-        String ans="";
-        int start=0,end=ch.length-1;
-        while (start<=end){
-            char temp=ch[start];
-            ch[start]=ch[end];
-            ch[end]=temp;
-            start+=1;
-            end-=1;
-        }
-        String s1=ch.toString();
-        return s1;
-    }
-
-    public static boolean validPalindrome(String s) {
-       int start=0,end=s.length()-1;
-       while (start<end){
-           if(s.charAt(start)==s.charAt(end)){
-               start+=1;
-               end-=1;
-           }else{
-               return isPallindromeDeletingOneElement(s,start+1,end)|| isPallindromeDeletingOneElement(s,start,end-1);
-           }
-       }
-       return true;
-    }
-
-    private static boolean isPallindromeDeletingOneElement(String s, int start, int end) {
-        while (start<end) {
-            if (s.charAt(start) == s.charAt(end)) {
-                start++;
-                end--;
-            }else{
-                return false;
-            }
-        }
-        return true;
-    }
+  
 }
