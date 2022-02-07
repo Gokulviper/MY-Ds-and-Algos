adc33e1277fcd00ed375708e172fd716f6be08f9
Gokulviper <deangokul57@gmail.com>
2022-02-07 16:06:59 +0530

    updated

--- src/String/Hashing.java
+++ src/String/Hashing.java
@@ -5,28 +5,51 @@ import java.util.*;
 public class Hashing {
     public static void main(String[] args) {
 
-        String ans[]=uncommonFromSentences("this apple is sweet","this apple is sour");
-       System.out.println(Arrays.toString(ans));
-        String s="hi this like the maths";
-        String[] r=s.split(" ");
-        System.out.println( Arrays.toString(r));
+        String ans[] = uncommonFromSentences("this apple is sweet", "this apple is sour");
+        System.out.println(Arrays.toString(ans));
+        String s = "hi this like the maths";
+        String[] r = s.split(" ");
+        System.out.println(Arrays.toString(r));
     }
+
     public static String[] uncommonFromSentences(String s1, String s2) {
 
-           Map<String,Integer> map=new HashMap<>();
-           for(String word:s1.split(" ")){
-               map.put(word,map.getOrDefault(word,0)+1);
-           } for(String word:s2.split(" ")){
-                map.put(word,map.getOrDefault(word,0)+1);
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : s1.split(" ")) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        for (String word : s2.split(" ")) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        List ans = new ArrayList();
+        for (String Word : map.keySet()) {
+            if (map.get(Word) == 1) {
+                ans.add(Word);
             }
-           List ans=new ArrayList();
-           for(String Word:map.keySet()){
-               if(map.get(Word)==1){
-                   ans.add(Word);
-               }
-           }
+        }
 
-         return (String[]) ans.toArray(new String[ans.size()]);
+        return (String[]) ans.toArray(new String[ans.size()]);
 
+    }
+
+    public static boolean wordPattern(String pattern, String word) {
+        String[] words = word.split(" ");  //push the all the words in string
+        Map<Character, String> map = new HashMap();
+        if (pattern.length() != words.length) return false; //edge case
+        for (int i = 0; i < pattern.length(); i++) {
+            char current_Char = pattern.charAt(i);
+            if (map.containsKey(current_Char)) {              //the map already have the value check are equal is
+                                                                    //is not equal return false
+                if (!map.get(current_Char).equals(words[i])) {
+                    return false;
+                }
+            } else {
+                if (map.containsValue(words[i])) {        //if the contains the value that not right because already have ha value if
+                    return false;                       //is must be passed we definitly return it
+                }
+                map.put(current_Char, words[i]);
+            }
+
+        }return true;
     }
 }
