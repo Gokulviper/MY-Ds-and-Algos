eee1783085806af3200c457c41d571c548b98152
Gokulviper <deangokul57@gmail.com>
2022-02-09 10:31:29 +0530

    updated

--- src/String/ConversionToIntegers.java
+++ src/String/ConversionToIntegers.java
@@ -12,45 +12,45 @@ public class ConversionToIntegers {
 
 
     public static String multiply(String num1, String num2) {
-        if(num1.equals("0") || num2.equals("0"))
-            return "0";
-        StringBuilder ans = new StringBuilder("0");
-        StringBuilder add = new StringBuilder();
-        for(int i = num1.length() - 1; i >= 0; i--){
-            int num = num1.charAt(i) - '0';
-            StringBuilder mul = multiply(num, num2);//multiplying last number to all the number in the other string
-            mul.append(add);//beacuse the adding last digit is nothing add to previous so the zero
-            add.append('0'); //every time we add 0
-            ans = sum(ans, mul);
+            if(num1.equals("0") || num2.equals("0"))
+                return "0";
+            StringBuilder ans = new StringBuilder("0");
+            StringBuilder add = new StringBuilder();
+            for(int i = num1.length() - 1; i >= 0; i--){
+                int num = num1.charAt(i) - '0';
+                StringBuilder mul = multiply(num, num2);//multiplying last number to all the number in the other string
+                mul.append(add);//beacuse the adding last digit is nothing add to previous so the zero
+                add.append('0'); //every time we add 0
+                ans = sum(ans, mul);
+            }
+            return ans.toString();
         }
-        return ans.toString();
-    }
-    public static StringBuilder multiply(int num, String str) {
-        StringBuilder sb = new StringBuilder();
-        int carry = 0;
-        for(int i = str.length() - 1; i >= 0; i--) {
-            int val = str.charAt(i) - '0';
-            int pro = num * val + carry;
-            sb.insert(0, pro % 10);
-            carry = pro / 10;
+        public static StringBuilder multiply(int num, String str) {
+            StringBuilder sb = new StringBuilder();
+            int carry = 0;
+            for(int i = str.length() - 1; i >= 0; i--) {
+                int val = str.charAt(i) - '0';
+                int pro = num * val + carry;
+                sb.insert(0, pro % 10);
+                carry = pro / 10;
+            }
+            if(carry > 0)
+                sb.insert(0, carry);
+            return sb;
         }
-        if(carry > 0)
-            sb.insert(0, carry);
-        return sb;
-    }
-    public static StringBuilder sum(StringBuilder num1, StringBuilder num2) {
-        StringBuilder ans = new StringBuilder();
-        int carry = 0;
-        int i = num1.length() - 1;
-        int j = num2.length() - 1;
-        while(i >= 0 || j >= 0 || carry > 0) {
-            int val1 = (i >= 0) ? num1.charAt(i--) - '0' : 0;
-            int val2 = (j >= 0) ? num2.charAt(j--) - '0' : 0;
-            int add = val1 + val2 + carry;
-            ans.insert(0, add % 10);
-            carry = add / 10;
-        }
-        return ans;
+        public static StringBuilder sum(StringBuilder num1, StringBuilder num2) {
+            StringBuilder ans = new StringBuilder();
+            int carry = 0;
+            int i = num1.length() - 1;
+            int j = num2.length() - 1;
+            while(i >= 0 || j >= 0 || carry > 0) {
+                int val1 = (i >= 0) ? num1.charAt(i--) - '0' : 0;
+                int val2 = (j >= 0) ? num2.charAt(j--) - '0' : 0;
+                int add = val1 + val2 + carry;
+                ans.insert(0, add % 10);
+                carry = add / 10;
+            }
+            return ans;
     }
 
     public static int myAtoi(String s) {
