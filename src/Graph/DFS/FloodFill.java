b447de7ccb136b936aff65082c862aa519254709
Gokulviper <deangokul57@gmail.com>
2022-02-25 18:21:12 +0530

    updated

--- src/Graph/DFS/FloodFill.java
+++ src/Graph/DFS/FloodFill.java
@@ -5,7 +5,9 @@ public class FloodFill {
         //Input: image = [[],[],[]],
         int[][]image={{1,1,1},{1,1,0},{1,0,1}};
         int sr = 1, sc = 1, newColor = 2;
-        flooodFill(image,sr,sc,newColor);
+      int[][]ans=  flooodFill(image,sr,sc,newColor);
+      print2dArray(ans);
+
     }
 
     private static int[][] flooodFill(int[][] image, int sr, int sc, int newColor) {
@@ -16,6 +18,22 @@ public class FloodFill {
     }
 
     private static void dfs(int[][] image, int sr, int sc, int color, int newColor) {
+        if(sr<0||sc<0||sr>=image.length||sc>=image[0].length||image[sr][sc]!=color){
+            return;
+        }
+        image[sr][sc]=newColor;
+        dfs(image,sr+1,sc,color,newColor);
+        dfs(image,sr-1,sc,color,newColor);
+        dfs(image,sr,sc+1,color,newColor);
+        dfs(image,sr,sc-1,color,newColor);
 
+    }
+    public static void print2dArray(int[][]nums){
+        for (int i=0;i<nums.length;i++){
+            for (int j=0;j<nums[0].length;j++){
+                System.out.print(nums[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
