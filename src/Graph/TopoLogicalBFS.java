de970e90e1f5be186bf649113cd6f043162d5fa7
Gokulviper <deangokul57@gmail.com>
2022-03-04 12:42:30 +0530

    updated

--- src/Graph/TopoLogicalBFS.java
+++ src/Graph/TopoLogicalBFS.java
@@ -2,14 +2,44 @@ package Graph;
 
 import java.util.LinkedList;
 import java.util.*;
+//khan's algorithm
 //find all all conection and make the connection of all nodes as marked
 //example 4 has 2 connection mark four as two conncetion
 //if any connection is zero take the value that is part of topo sort
 //make bfs and if the element is zero push the queue that elemnet when out that is part of topo sort
-
+//tc-o(n+e)
+//sc-o(n)+0(n)
 public class TopoLogicalBFS {
+    public boolean isCycli1c(int N, LinkedList<Integer>[]adj) {
+        Queue<Integer>queue=new LinkedList<>();
+        int[]indegeree=new int[N];
+        for (int i=0;i<N;i++){
+            for (int v:adj[i]) {
+                indegeree[v]++;
+            }
+        }
+        int count=0;
+        for (int i=0;i<N;i++){
+            if (indegeree[i]==0){
+                queue.add(i);
+            }
+        }
+        while (queue.isEmpty()){
+            int u= queue.poll();
+            for (int v:adj[u]){
+                indegeree[v]--;
+                if (indegeree[v]==0){
+                    queue.add(v);
+                }
+            }
+        }
+        if (count==N)return false;
+        return true;
+
+
+    }
     public boolean isCyclic(int N, LinkedList<Integer>[]adj) {
-        int topo[] = new int[N];
+
         int indegree[] = new int[N];
         for(int i = 0;i<N;i++) {
             for(Integer it: adj[i]) {
--- src/Stack/RemoveAllAdjacentDuplicatesInString.java
+++ src/Stack/RemoveAllAdjacentDuplicatesInString.java
@@ -1,11 +1,66 @@
 package Stack;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 public class RemoveAllAdjacentDuplicatesInString {
     public static void main(String[] args) {
-        System.out.println(removeDuplicates("abbaca"));
+        //"pbbcggttciiippooaais"
+        //2
+        System.out.println(removeDuplicates("pbbcggttciiippooaais",2));
+    }
+    public static String removeDuplicates(String s, int k) {
+        Stack<int[]>stack=new Stack<>();
+        for (char ch:s.toCharArray()){
+            if(!stack.isEmpty()&& stack.peek()[0]==ch){
+                stack.peek()[1]++;
+            }else{
+                stack.push(new int[]{ch,1});
+            }
+            if (stack.peek()[1]==k){
+                stack.pop();
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        while (!stack.isEmpty()){
+            int[]top=stack.pop();
+            while (top[1]-->0){
+                sb.append((char) top[0]);
+            }
+        }
+        return sb.reverse().toString();
+    }
+    public static String removeDuplicatesNotWorkingAllTestCases(String s, int k) {
+        //this is a  medium problem
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (char ch : map.keySet()) {
+            int val = map.get(ch);
+            if (val < k) continue;
+            if (val>k&&val%k==0){
+                val = val % k - k;
+            }else {
+               val=val-k;
+            }
+            if (val == 0) {
+                map.put(ch, val);
+            } else {
+                map.put(ch, val);
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        for (char ch:s.toCharArray()){
+            if (map.get(ch)>0){
+                sb.append(ch);
+                map.put(ch,map.getOrDefault(ch,0)-1);
+            }
+        }
+        return sb.toString();
     }
 
     public static String removeDuplicates(String str) {
