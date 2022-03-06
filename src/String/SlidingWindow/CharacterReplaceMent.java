ff7e07b7a1fbe42c2907719f236f81c60227823e
Gokulviper <deangokul57@gmail.com>
2022-03-06 12:10:05 +0530

    updated

--- src/String/SlidingWindow/CharacterReplaceMent.java
+++ src/String/SlidingWindow/CharacterReplaceMent.java
@@ -6,7 +6,10 @@ class CharacterReplaceMent {
     public static void main(String[] args) {
 String  s = "AABABBA";int k = 1;
         System.out.println(characterReplacement(s,k));
+        int a=2,b=7,c=10;
+
     }
+
     public static int characterReplacement(String s, int k) {
         Map<Character, Integer> map = new HashMap<>();
         int i = 0;
