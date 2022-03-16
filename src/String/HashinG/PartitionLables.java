a4d995c6c3fb09fb8812b32d61861725623e201e
Gokulviper <deangokul57@gmail.com>
2022-03-16 18:22:59 +0530

    updated

--- src/String/HashinG/PartitionLables.java
+++ src/String/HashinG/PartitionLables.java
@@ -15,7 +15,11 @@ class PartitionLables {
         List<Integer>ans=new ArrayList<>();
         int start=0,end=0;
         for (int i=0;i<s.length();i++){
-            end=Math.max(end,s.charAt(i)-'a');
+            end=Math.max(end,last_index[s.charAt(i)-'a']);
+            if (i==end){
+                ans.add(end-start+1);
+                start=i+1;
+            }
         }
         return ans;
     }
