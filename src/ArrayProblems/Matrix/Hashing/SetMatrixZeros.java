7758b8a7e8499487e75d279b1e6b50792855bc51
Gokulviper <deangokul57@gmail.com>
2022-03-17 18:39:34 +0530

    updated

--- src/ArrayProblems/Matrix/Hashing/SetMatrixZeros.java
+++ src/ArrayProblems/Matrix/Hashing/SetMatrixZeros.java
@@ -6,36 +6,11 @@ import java.util.Set;
 public class SetMatrixZeros {
     public static void main(String[] args) {
         int[][]matrix = {{0,1,1},{1,0,1},{1,1,1}};
-        setZeroes1(matrix);
+        setZeroes(matrix);
         print(matrix);
 
     }
-    public static void setZeroes1(int[][] matrix) {
-        Boolean isCol = false;
-        int R = matrix.length;
-        int C = matrix[0].length;
-        for (int i = 0; i < R; i++) {
-            if (matrix[i][0] == 0) {
-                isCol = true;
-            }
-            for (int j = 1; j < C; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[0][j] = 0;
-                    matrix[i][0] = 0;
-                }}  }
-        for (int i = 1; i<R; i++) {
-            for (int j = 1; j < C; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }} }
-        if (matrix[0][0] == 0) {
-            for (int j = 0; j < C; j++) {
-                matrix[0][j] = 0;
-            }  }
-        if (isCol) {
-            for (int i = 0; i < R; i++) {
-                matrix[i][0] = 0;
-            } } }
+
 
         public static void setZeroes(int[][] matrix) {
 Boolean isCal=false;
