f463f6edcb4dcd85fa91a950b414dd42180a8a68
Gokulviper <deangokul57@gmail.com>
2022-03-26 17:39:13 +0530

    updated

---
+++ src/ArrayProblems/SubArray/Solution.java
@@ -0,0 +1,20 @@
+package ArrayProblems.SubArray;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                int sum = 0;
+                for (int i = start; i < end; i++)
+                    sum += nums[i];
+                if (sum == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
