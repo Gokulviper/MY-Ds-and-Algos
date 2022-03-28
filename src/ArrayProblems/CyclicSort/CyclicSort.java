9aade75b6676a4c24acd13e774a38fea58299f72
Gokulviper <deangokul57@gmail.com>
2022-03-28 19:54:14 +0530

    updated

---
+++ src/ArrayProblems/CyclicSort/CyclicSort.java
@@ -0,0 +1,7 @@
+package ArrayProblems.CyclicSort;
+//hint of cyclic 1 to n
+public class CyclicSort {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ArrayProblems/LargestNumber.java
@@ -0,0 +1,16 @@
+package ArrayProblems;
+
+class LargestNumber {
+    public static void main(String[] args) {
+        int nums[]={3,30,34,5,9};
+        System.out.println((3*100)+30);
+        System.out.println(30+(3*100));
+    }
+    public static String largestNumber(int[] nums) {
+        int com1=0,com2=0,ans=0;
+        for (int i=0;i<nums.length;i++){
+
+        }
+        return "";
+    }
+}
