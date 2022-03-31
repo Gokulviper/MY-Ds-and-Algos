9aa7931ae4be95da7c0402e305f650db14a38123
Gokulviper <deangokul57@gmail.com>
2022-03-31 12:37:20 +0530

    updated

---
+++ src/ArrayProblems/Sorting/GeneralAlgorithms/SelectionSort.java
@@ -0,0 +1,7 @@
+package ArrayProblems.Sorting.GeneralAlgorithms;
+//select the largest element in the array put at the correct index
+public class SelectionSort {
+    public static void main(String[] args) {
+
+    }
+}
