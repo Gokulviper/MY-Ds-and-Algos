aa12155da02c60a6624ab82ebbd50d9371bdb795
Gokulviper <deangokul57@gmail.com>
2022-03-31 14:41:37 +0530

    updated

---
+++ src/ArrayProblems/twoPointer/NextPermutation.java
@@ -0,0 +1,11 @@
+package ArrayProblems.twoPointer;
+//swap the pair having lowest weightage to get next greater sequence
+//special cases:
+//case1 :they give the decending array:sort the arrray
+//case2:the array is sorted:swap the last 2 elements
+//other cases :mixed array:swap only using the last asc subarray BEAUSE any element before it will higher weigtage
+public class NextPermutation {
+    public static void main(String[] args) {
+
+    }
+}
