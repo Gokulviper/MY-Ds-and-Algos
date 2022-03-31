0def1dc9d82ce30ce31b7dd8162d873b551284bc
Gokulviper <deangokul57@gmail.com>
2022-03-31 12:12:16 +0530

    updated

--- src/ArrayProblems/Sorting/GeneralAlgorithms/BubbleSort.java
+++ src/ArrayProblems/Sorting/GeneralAlgorithms/BubbleSort.java
@@ -1,6 +1,6 @@
 package ArrayProblems.Sorting.GeneralAlgorithms;
 //also calles as sinking sort/exchange sort
-//after the first pass the largest element comes to end
+
 //inplace sorting algorithm  - constant  space algorithms
 
 import java.util.Arrays;
@@ -19,16 +19,18 @@ public class BubbleSort {
     public static void bubbleSort(int[]nums){
 
         for (int i = 0; i < nums.length ; i++) {
-            boolean   swaped=false;
+            boolean   swapped=false;
             for (int j = 1; j < nums.length-i; j++) {
+                //after the first pass the largest element comes to end
+                //so wre dont check the last exceted so we put nums.length-i
                 if(nums[j]<nums[j-1]){
                     int t=nums[j];
                     nums[j]=nums[j-1];
                     nums[j-1]=t;
-                    swaped=true;
+                    swapped=true;
                 }
             }
-            if (!swaped){
+            if (!swapped){
                 break;
             }
         }
