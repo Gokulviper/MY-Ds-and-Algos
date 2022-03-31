d27216f9c3e3269d350a7c9677184c31d8285163
Gokulviper <deangokul57@gmail.com>
2022-03-31 13:01:06 +0530

    updated

--- src/ArrayProblems/Sorting/GeneralAlgorithms/SelectionSort.java
+++ src/ArrayProblems/Sorting/GeneralAlgorithms/SelectionSort.java
@@ -1,7 +1,13 @@
 package ArrayProblems.Sorting.GeneralAlgorithms;
 //select the largest element in the array put at the correct index
+//best case: n^2
+//worst case: n^2
+//its perform well in small list and arrays
 public class SelectionSort {
     public static void main(String[] args) {
+
+    }
+    static void selectionSort(int[]nums){
 
     }
 }
