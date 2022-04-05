fc9a83384488199d2ddf44aca56e2db26a2b92fc
Gokulviper <deangokul57@gmail.com>
2022-04-05 18:05:29 +0530

    updated

--- src/ArrayProblems/Sorting/IntervalsProblems/MergeIntervals.java
+++ src/ArrayProblems/Sorting/IntervalsProblems/MergeIntervals.java
@@ -9,17 +9,17 @@ public class MergeIntervals {
      int[][]nums={{1,3},{2,6},{8,10},{15,18}};
      merge(nums);
     }
-    public static int[][] merge(int[][]intervals){
+    public static int[][] merge(int[][]nums){
        List<int[]>ans=new ArrayList<>();
-       Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-       int start=intervals[0][0],end=intervals[0][1];
-        for (int i = 0; i <intervals.length ; i++) {
-            if (intervals[i][0]<=end){
-                end=Math.max(end,intervals[i][1]);
+       Arrays.sort(nums,(a,b)->a[0]-b[0]);
+       int start=nums[0][0],end=nums[0][1];
+        for (int j = 0; j <nums.length ; j++) {
+            if (nums[j][0]<=end){
+                end=Math.max(end,nums[j][1]);
             }else {
                 ans.add(new int[]{start,end});
-                start=intervals[i][0];
-                end=intervals[i][1];
+                start=nums[j][0];
+                end=nums[j][1];
             }
         }
         ans.add(new int[]{start,end});
