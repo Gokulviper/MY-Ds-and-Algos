79b79adb3684f693e7aa4c8d766bf4e9a181d24e
Gokulviper <deangokul57@gmail.com>
2022-04-06 19:21:48 +0530

    updated

---
+++ src/Recursion/BasicPrograms.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class BasicPrograms {
+    public static void main(String[] args) {
+        printNumbers(5);
+    }
+
+    private static void printNumbers(int n) {
+        if (n<=0){
+            return;
+        }
+        printNumbers(n-1);
+        System.out.println(n);
+    }
+}
