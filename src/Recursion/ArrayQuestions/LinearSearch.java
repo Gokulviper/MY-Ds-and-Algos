18f77c1a0c07d286767922a68689339b0688ca38
Gokulviper <deangokul57@gmail.com>
2022-04-15 11:51:27 +0530

    updated

--- src/Recursion/ArrayQuestions/LinearSearch.java
+++ src/Recursion/ArrayQuestions/LinearSearch.java
@@ -1,10 +1,26 @@
 package Recursion.ArrayQuestions;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int[]nums={5,8,3,8,0,3};
-        System.out.println(linearSearch(nums,0,8));
+        int[]nums={8,8,3,8,8,3};
+        System.out.println(linearSearchReturnList(nums,0,8));
     }
+
+    private static ArrayList<Integer> linearSearchReturnList(int[] nums, int i, int target) {
+        ArrayList<Integer> list=new ArrayList<>();
+        if (i==nums.length){
+            return list;
+        }
+        if (nums[i]==target){
+            list.add(i);
+        }
+        ArrayList<Integer> ansBelowCalls=linearSearchReturnList(nums,i+1,target);
+        list.addAll(ansBelowCalls);
+        return list;
+    }
+
     private static int linearSearch(int[] nums,int i,int target) {
         if (i==nums.length){
             return -1;
