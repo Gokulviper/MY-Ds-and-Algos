a158756367610fa178ce6594fd276a7560c7893e
Gokulviper <deangokul57@gmail.com>
2022-04-15 09:26:19 +0530

    updated

---
+++ src/Recursion/ArrayQuestions/LinearSearch.java
@@ -0,0 +1,18 @@
+package Recursion.ArrayQuestions;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[]nums={5,8,3,8,0,3};
+        System.out.println(linearSearch(nums,0,8));
+    }
+
+    private static int linearSearch(int[] nums,int i,int target) {
+        if (i==nums.length){
+            return -1;
+        }
+        if (nums[i]==target){
+            return i;
+        }
+        return linearSearch(nums,i+1,target);
+    }
+}
