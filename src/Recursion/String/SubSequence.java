9a89a9ccc5d86a6921fff23dfc9c2aaf817ede7b
Gokulviper <deangokul57@gmail.com>
2022-04-17 16:03:15 +0530

    updated

--- src/Recursion/String/SubSequence.java
+++ src/Recursion/String/SubSequence.java
@@ -1,7 +1,16 @@
 package Recursion.String;
-
+import java.util.ArrayList;
 public class SubSequence {
     public static void main(String[] args) {
-
+        subseq("abc", "", new ArrayList<>());
+    }
+    private static ArrayList subseq(String s, String ans, ArrayList list) {
+        if (s.isEmpty()){
+            list.add(ans);
+            return list;
+        }
+        subseq(s.substring(1),ans+s.charAt(0),list);
+        subseq(s.substring(1),ans,list);
+             return list;
     }
 }
