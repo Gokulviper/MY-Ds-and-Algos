8a990f6ad4c18a5600cd6136fff7ffa3fbc46f99
Gokulviper <deangokul57@gmail.com>
2022-04-25 18:27:55 +0530

    updated

--- src/Recursion/ArrayQuestions/Subsequence.java
+++ src/Recursion/ArrayQuestions/Subsequence.java
@@ -8,6 +8,26 @@ public class Subsequence {
 
         System.out.println( subset(new int[]{1,2,3}));
     }
+    private static List<List<Integer>> subsets(int[] nums) {
+        List ans=new ArrayList();
+        List list=new ArrayList();
+
+            sum(nums,0,list,ans);
+            return ans;
+    }
+
+    private static void sum(int[] nums, int index, List list, List ans) {
+        if (list.size()<=nums.length){
+            ans.add(new ArrayList<>(list));
+
+        }
+        for (int i = index; i <nums.length ; i++) {
+            list.add(nums[i]);
+            sum(nums,i+1,list,ans);
+            list.remove(list.size()-1);
+        }
+    }
+
 
     private static List<List<Integer>> subset(int[] nums) {
         List<List<Integer> >outer=new ArrayList<>();
