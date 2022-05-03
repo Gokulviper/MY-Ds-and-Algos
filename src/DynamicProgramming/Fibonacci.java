bd08759a2f4b7c9dfa8c1b5991fd73dbfd14e059
Gokulviper <deangokul57@gmail.com>
2022-05-03 10:40:22 +0530

    updated

--- src/DynamicProgramming/Fibonacci.java
+++ src/DynamicProgramming/Fibonacci.java
@@ -2,8 +2,39 @@ package DynamicProgramming;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int n=4;
-        System.out.println(bottomUp(n));
+        int n=50;
+        System.out.println(spaceOptmized(n));
+    }
+
+    private static int NoramRecursion(int n) {
+        if (n<=1){
+            return n;
+        }
+        return NoramRecursion(n-1)+NoramRecursion(n-2);
+    }
+
+    private static int spaceOptmized(int n) {
+        int prev=1;
+        int s_prev=0;
+        int f=0;
+        for (int i = 2; i <n+1 ; i++) {
+            f=prev+s_prev;
+
+            s_prev=prev;
+            prev=f;
+        }
+        return f;
+    }
+
+    private static int topDown(int n, int[] dp) {
+     if (dp[n]==0){
+         if (n<2){
+             dp[n]=n;
+         }else {
+             dp[n]=topDown(n-1,dp)+topDown(n-2,dp);
+         }
+     }
+return dp[n];
     }
 
     private static int bottomUp(int n) {
@@ -14,4 +45,5 @@ public class Fibonacci {
         }
         return dp[n-1];
     }
+
 }
