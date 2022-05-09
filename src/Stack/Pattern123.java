3667faed174ef6b9607b540aacad295bc4f7338c
Gokulviper <deangokul57@gmail.com>
2022-05-09 17:20:48 +0530

    updated

--- src/Stack/Pattern123.java
+++ src/Stack/Pattern123.java
@@ -9,7 +9,7 @@ class Pattern123 {
     }
     public static boolean find132pattern(int[] nums) {
     int s3=Integer.MIN_VALUE;
-        Stack<Integer> stack=new Stack();
+        Stack<Integer> stack=new Stack<>();
         for(int i=nums.length-1;i>=0;i--){
             if(nums[i]<s3){
                 return true;
