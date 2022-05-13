17e12d5ce5da78af0940fc3230dcc272251593e5
Gokulviper <deangokul57@gmail.com>
2022-05-13 17:42:45 +0530

    updated

--- src/DynamicProgramming/SubSequence/SubsetSumEqulasK.java
+++ src/DynamicProgramming/SubSequence/SubsetSumEqulasK.java
@@ -10,8 +10,36 @@ public class SubsetSumEqulasK {
         for (int[]row:dp){
             Arrays.fill(row,-1);
         }
-        System.out.println(memoriazation(nums.length-1,k,nums,dp));
+        System.out.println(Tabulation(nums.length,k,nums));
     }
+    static boolean Tabulation(int n, int k,int[] arr){
+
+
+        boolean dp[][]= new boolean[n][k+1];
+        for(int i=0; i<n; i++){
+            dp[i][0] = true;
+        }
+
+        if(arr[0]<=k)
+            dp[0][arr[0]] = true;
+
+        for(int ind = 1; ind<n; ind++){
+            for(int target= 1; target<=k; target++){
+
+                boolean notTaken = dp[ind-1][target];
+
+                boolean taken = false;
+                if(arr[ind]<=target)
+                    taken = dp[ind-1][target-arr[ind]];
+
+                dp[ind][target]= notTaken||taken;
+            }
+        }
+
+        return dp[n-1][k];
+    }
+
+
 
     private static boolean memoriazation(int ind, int target,int[] arr, int[][] dp) {
         if(target==0)
