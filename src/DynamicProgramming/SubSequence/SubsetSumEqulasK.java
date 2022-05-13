649a5cefc5df4e5585cf41594855086f689cb57e
Gokulviper <deangokul57@gmail.com>
2022-05-13 16:54:56 +0530

    updated

--- src/DynamicProgramming/SubSequence/SubsetSumEqulasK.java
+++ src/DynamicProgramming/SubSequence/SubsetSumEqulasK.java
@@ -1,9 +1,35 @@
 package DynamicProgramming.SubSequence;
 
+import java.util.Arrays;
+
 public class SubsetSumEqulasK {
     public static void main(String[] args) {
         int[]nums={3,5,6,7};
-        System.out.println(f(nums,nums.length-1,15));
+        int k=34;
+        int[][]dp=new int[nums.length][k+1];
+        for (int[]row:dp){
+            Arrays.fill(row,-1);
+        }
+        System.out.println(memoriazation(nums.length-1,k,nums,dp));
+    }
+
+    private static boolean memoriazation(int ind, int target,int[] arr, int[][] dp) {
+        if(target==0)
+            return true;
+
+        if(ind == 0)
+            return arr[0] == target;
+
+        if(dp[ind][target]!=-1)
+            return dp[ind][target] != 0;
+
+        boolean notTaken = memoriazation(ind-1,target,arr,dp);
+
+        boolean taken = false;
+        if(arr[ind]<=target)
+            taken = memoriazation(ind-1,target-arr[ind],arr,dp);
+        dp[ind][target]=notTaken||taken?1:0;
+        return notTaken||taken;
     }
 
     private static boolean f(int[] nums, int index, int target) {
