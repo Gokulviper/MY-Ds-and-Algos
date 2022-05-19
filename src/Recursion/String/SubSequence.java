ba36d2d2e9ed89d661c853e4748d8c872e820318
Gokulviper <deangokul57@gmail.com>
2022-05-19 19:35:51 +0530

    updated

--- src/Recursion/String/SubSequence.java
+++ src/Recursion/String/SubSequence.java
@@ -1,8 +1,20 @@
 package Recursion.String;
 import java.util.ArrayList;
+import java.util.List;
+
 public class SubSequence {
     public static void main(String[] args) {
-        System.out.println(subseq("abc", "", new ArrayList<>()));
+        System.out.println(allSub("abc", "", new ArrayList<>()));
+    }
+    private static List<String> allSub(String s,String ans,List<String>list){
+        if (s.isEmpty()){
+         list.add(ans);
+            return list;
+        }
+        allSub(s.substring(1),ans+s.charAt(0),list);
+        allSub(s.substring(1),ans,list);
+        return list;
+
     }
     private static ArrayList subseq(String s, String ans, ArrayList list) {
         if (s.isEmpty()){
