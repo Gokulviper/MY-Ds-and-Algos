0afa93066e21632b289fcf97c69c55906f63c696
Gokulviper <deangokul57@gmail.com>
2022-05-20 12:10:47 +0530

    updated

--- src/DynamicProgramming/Array/TargetSum.java
+++ src/DynamicProgramming/Array/TargetSum.java
@@ -8,7 +8,59 @@ class TargetSum {
 
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return targetSum(nums.length, target, nums);
+        return tabluationChecl( target, nums);
+    }
+
+    private int tabluationChecl(int target, int[] nums) {
+        int sum=0;
+        for (int num:nums){
+            sum+=num;
+        }
+        if (sum-target<0||(sum-target)%2==1)return 0;
+        return spaceOprtimized((sum-target)/2,nums);
+    }
+
+    private int spaceOprtimized(int tar, int[] num) {
+        int n = num.length;
+
+        int prev[]=new int[tar+1];
+
+        if(num[0] == 0) prev[0] =2;  // 2 cases -pick and not pick
+        else prev[0] = 1;  // 1 case - not pick
+
+        if(num[0]!=0 && num[0]<=tar) prev[num[0]] = 1;  // 1 case -pick
+
+        for(int ind = 1; ind<n; ind++){
+            int cur[]=new int[tar+1];
+            for(int target= 0; target<=tar; target++){
+                int notTaken = prev[target];
+
+                int taken = 0;
+                if(num[ind]<=target)
+                    taken = prev[target-num[ind]];
+
+                cur[target]= (notTaken + taken);
+            }
+            prev = cur;
+        }
+        return prev[tar];
+    }
+
+    private int tabluation(int target, int[] nums) {
+        int[][]dp=new int[nums.length][target+1];
+      dp[0][0]=nums[0]==0?2:1;
+      if (nums[0]!=0&&nums[0]<=target)dp[0][nums[0]]=1;
+        for (int i = 1; i <nums.length ; i++) {
+            for (int j = 0; j <=target ; j++) {
+                 int not_pick=dp[i-1][j];
+                 int pick=0;
+                 if(nums[i]<=j){
+                     pick=dp[i-1][j-nums[i]];
+                 }
+                 dp[i][j]=pick+not_pick;
+            }
+        }
+        return dp[nums.length-1][target];
     }
 
     private int targetSum(int n, int target, int[] nums) {
@@ -29,7 +81,7 @@ class TargetSum {
         }
         int not_pick=find(index-1,target,nums);
         int pick=0;
-        if(nums[0]<=target){
+        if(nums[index]<=target){
             pick=find(index-1,target-nums[index],nums);
         }
         return pick+not_pick;
