45085d0adbd4f30c9af274946dce74d95ae39881
Gokulviper <deangokul57@gmail.com>
2022-05-22 15:31:47 +0530

    updated

--- src/DynamicProgramming/String/LongestCommonSubsequence.java
+++ src/DynamicProgramming/String/LongestCommonSubsequence.java
@@ -13,7 +13,6 @@ public class LongestCommonSubsequence {
         int n=s1.length(),m=s2.length();
         int[][]dp=new int[n+1][m+1];
 int []prev=new int[m+1],cur=new int[m+1];
-
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s1.charAt(i-1)==s2.charAt(j-1))cur[j]=1+prev[j-1];
