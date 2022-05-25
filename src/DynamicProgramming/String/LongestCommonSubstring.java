aaa5cfea159c7df7024787d25ac43ac02998ccf7
Gokulviper <deangokul57@gmail.com>
2022-05-25 20:05:40 +0530

    updated

--- src/DynamicProgramming/String/LongestCommonSubstring.java
+++ src/DynamicProgramming/String/LongestCommonSubstring.java
@@ -8,20 +8,7 @@ public class LongestCommonSubstring {
     public static void main(String[] args) {
         System.out.println(lcs("abefd", "gfabeg"));
     }
-    static int lc1s(String s1, String s2){
-        int n=s1.length(),m=s2.length();
-        int[][]dp=new int[n+1][m+1];
-        int ans=0;
-        for (int i = 1; i <=n ; i++) {
-            for (int j = 1; j <=m ; j++) {
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                ans=Math.max(ans,1+dp[i][j]);
-                }else dp[i][j]=0;
-            }
-        }
-        return ans;
-    }
+
         static int lcs(String s1, String s2){
 int n=s1.length();
 int m=s2.length();
