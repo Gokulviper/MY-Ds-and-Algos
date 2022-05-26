2d441db6d536954a5d562bbdfe2c358317ec26da
Gokulviper <deangokul57@gmail.com>
2022-05-26 17:56:37 +0530

    updated

--- src/ArrayProblems/SubArray/ShortestUnSortedContinuouesSubArray.java
+++ src/ArrayProblems/SubArray/ShortestUnSortedContinuouesSubArray.java
@@ -85,5 +85,36 @@ public class ShortestUnSortedContinuouesSubArray {
         }
         return r - l > 0 ? r - l + 1 : 0;
     }
+    public int findUnsortedSubarrayBrute(int[] nums) {
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j <= nums.length; j++) {
+                int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, prev = Integer.MIN_VALUE;
+                for (int k = i; k < j; k++) {
+                    min = Math.min(min, nums[k]);
+                    max = Math.max(max, nums[k]);
+                }
+                if ((i > 0 && nums[i - 1] > min) || (j < nums.length && nums[j] < max))
+                    continue;
+                int k = 0;
+                while (k < i && prev <= nums[k]) {
+                    prev = nums[k];
+                    k++;
+                }
+                if (k != i)
+                    continue;
+                k = j;
+                while (k < nums.length && prev <= nums[k]) {
+                    prev = nums[k];
+                    k++;
+                }
+                if (k == nums.length) {
+                    res = Math.min(res, j - i);
+
+                }
+            }
+        }
+        return res;
+    }
 
 }
