22f645a0b4166a4168cbc7bbcb28b57e91cae5b9
Gokulviper <deangokul57@gmail.com>
2022-05-26 16:38:27 +0530

    updated

---
+++ src/DynamicProgramming/String/matching/WildCardMatching.java
@@ -0,0 +1,44 @@
+package DynamicProgramming.String.matching;
+
+import java.util.Arrays;
+
+class WildCardMatching {
+    public boolean isMatch(String S1, String S2) {
+        int n = S1.length();
+        int m = S2.length();
+
+        boolean dp[][] = new boolean[n][m];
+
+        return wildcardMatchingUtil(S1, S2, n - 1, m - 1, dp);
+    }
+    static boolean isAllStars(String S1, int i) {
+        for (int j = 0; j <= i; j++) {
+            if (S1.charAt(j) != '*')
+                return false;
+        }
+        return true;
+    }
+
+    static boolean wildcardMatchingUtil(String S1, String S2, int i, int j, boolean[][] dp) {
+
+        //Base Conditions
+        if (i < 0 && j < 0)
+            return true;
+        if (j < 0)
+            return false;
+        if (i < 0)
+            return isAllStars(S2, j) ;
+
+        if (!dp[i][j] ) return dp[i][j];
+
+        if (S1.charAt(i) == S2.charAt(j) || S2.charAt(j) == '?')
+            return dp[i][j] = wildcardMatchingUtil(S1, S2, i - 1, j - 1, dp);
+
+        else {
+            if (S2.charAt(j) == '*')
+                return (wildcardMatchingUtil(S1, S2, i - 1, j, dp)) || (wildcardMatchingUtil(S1, S2, i, j - 1, dp) );
+            else return false;
+        }
+    }
+
+}
--- src/Stack/DailyTempratures.java
+++ src/Stack/DailyTempratures.java
@@ -1,11 +1,13 @@
 package Stack;
 
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class DailyTempratures {
     public static void main(String[] args) {
-
+int[]nums={73,74,75,71,69,72,76,73};
+        Arrays.toString(dailyTemperatures(nums));
     }
     public static int[] dailyTemperatures(int[] nums) {
         int n=nums.length;
@@ -20,5 +22,27 @@ public class DailyTempratures {
         }
         return ans;
 
+    }
+    public int[] dailyTemperaturesspace(int[] temperatures) {
+        int n = temperatures.length;
+        int hottest = 0;
+        int answer[] = new int[n];
+
+        for (int currDay = n - 1; currDay >= 0; currDay--) {
+            int currentTemp = temperatures[currDay];
+            if (currentTemp >= hottest) {
+                hottest = currentTemp;
+                continue;
+            }
+
+            int days = 1;
+            while (temperatures[currDay + days] <= currentTemp) {
+                // Use information from answer to search for the next warmer day
+                days += answer[currDay + days];
+            }
+            answer[currDay] = days;
+        }
+
+        return answer;
     }
 }
