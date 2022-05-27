6b77dc7b66c38b8c0d73c90b694b1ac688f7ed81
Gokulviper <deangokul57@gmail.com>
2022-05-27 17:56:36 +0530

    updated

--- src/DynamicProgramming/Array/subArray/MaximumProductSubArrray.java
+++ src/DynamicProgramming/Array/subArray/MaximumProductSubArrray.java
@@ -1,11 +1,17 @@
 package DynamicProgramming.Array.subArray;
 
 class MaximumProductSubArrray {
- /*   If this question consist of only positive integer, then this question would be very easy, just return the product of all integer.
+ /*   If this question consist of only positive integer, then this question would be very easy,
+  just return the product of all integer.
 
-However, it contain negative integer, while negative - negative product would result in a positive number, so we can't return product of all integer, or just calculate the product of positive integer.
+However, it contain negative integer,
+ while negative - negative product would result in a positive number, so we can't return product of all integer,
+  or just calculate the product of positive integer.
 
-Therefore, we create two dp, one save the maximum product from 0 to i, and the other one save the minimum product from 0 to i. And for each dp update, we calculate the product of both maximum product * nums[i] and minimum prodcut * nums[i], just in case if nums[i] is negative and it form a larger positive product when minimum prodcut * nums[i].*/
+Therefore, we create two dp, one save the maximum product from 0 to i,
+and the other one save the minimum product from 0 to i. And for each dp update,
+ we calculate the product of both maximum product * nums[i] and minimum prodcut * nums[i], just in case
+ if nums[i] is negative and it form a larger positive product when minimum prodcut * nums[i].*/
     public int maxProduct(int[] nums) {
         int[]max=new int[nums.length];
         int []min=new int[nums.length];
