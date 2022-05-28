dda13364bf1e7ec209d812f4f2564c7bc9770d88
Gokulviper <deangokul57@gmail.com>
2022-05-28 17:05:13 +0530

    updated

--- src/DynamicProgramming/String/LongestCommonSubstring.java
+++ src/DynamicProgramming/String/LongestCommonSubstring.java
@@ -10,10 +10,10 @@ public class LongestCommonSubstring {
     }
 
         static int lcs(String s1, String s2){
-int n=s1.length();
-int m=s2.length();
-int[][]dp=new int[n+1][m+1];
-int ans=0;
+   int n=s1.length();
+   int m=s2.length();
+   int[][]dp=new int[n+1][m+1];
+   int ans=0;
             for (int i = 1; i <=n ; i++) {
                 for (int j = 1; j <=m ; j++) {
                     if (s1.charAt(i-1)==s2.charAt(j-1)){
