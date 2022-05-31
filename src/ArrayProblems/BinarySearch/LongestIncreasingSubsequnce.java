7c967aa94bfe773964637d8761ad2ae630b3faf0
Gokulviper <deangokul57@gmail.com>
2022-05-31 11:57:12 +0530

    updated

--- src/ArrayProblems/BinarySearch/LongestIncreasingSubsequnce.java
+++ src/ArrayProblems/BinarySearch/LongestIncreasingSubsequnce.java
@@ -2,32 +2,35 @@ package ArrayProblems.BinarySearch;
 
 public class LongestIncreasingSubsequnce {
     public static void main(String[] args) {
-
+      int[]nums={10,9,2,5,3,7,101,18};
+        System.out.println( lengthOfLIS(nums));
     }
+    /*idea is the current number is the part of sequence nums[i]>tail[j-1]
+    then add your sequnce is it smaller number check ceiling alorithm
+    //because the increasing sequence is sorted so find the the current index
+    which is the correct position
+    */
 
-    public int lengthOfLIS(int[] arr) {
+    public static int lengthOfLIS(int[] arr) {
         // optimized one
-
         int n = arr.length;
-        if (n == 0)
-            return 0;
+        if (n == 0) return 0;
         int[] tail = new int[n];
         tail[0] = arr[0];
-        int length = 1;
-
+        int j = 1;//this is length of the longest sequence
         for (int i = 1; i < n; i++) {
-            if (arr[i] > tail[length - 1]){
-                tail[length] = arr[i];
-                length++;
+            if (arr[i] > tail[j - 1]){
+                tail[j] = arr[i];
+                j++;
             }else {
-                int ceil = LIS_Ceil(tail, length - 1, arr[i]);
+                int ceil = ceil(tail, j - 1, arr[i]);
+                //ceiling is next  ggreater number
                 tail[ceil] = arr[i];
             }
         }
-        return length;
+        return j;
     }
-
-    private int LIS_Ceil(int[] arr, int high, int ele){
+    private static int ceil(int[] arr, int high, int ele){
         int low = 0;
         while (low < high){
             int mid = low + (high - low) / 2;
