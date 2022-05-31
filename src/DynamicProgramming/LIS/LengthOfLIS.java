8ea0741fca940020931ca2fdd74963c5c97de2c0
Gokulviper <deangokul57@gmail.com>
2022-05-31 09:55:55 +0530

    updated

--- src/DynamicProgramming/LIS/LengthOfLIS.java
+++ src/DynamicProgramming/LIS/LengthOfLIS.java
@@ -1,15 +1,50 @@
 package DynamicProgramming.LIS;
 
+import java.util.Arrays;
+
 class LengthOfLIS {
     public static void main(String[] args) {
 
+    }
+    public int singleArrayspace(int[] nums) {
+       int[]dp=new int[nums.length];
+        Arrays.fill(dp,1);//beacuse the lowest increasing subseqence length is 1
+int max=0;
+        for (int i=0;i<nums.length;i++){
+            for (int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],1+dp[j]);
+                }
+            }
+            max=Math.max(max,dp[i]);}
+        return max;
+    }
+
+    public int space(int[] nums) {
+        int[]prev_row=new int[nums.length],cur_row=new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
+            //prev index is previous of curent so you take  is 1 based indexing
+            //you avoid is condition is -1 so you put +1 it could be zero
+            for(int prev=i-1;prev>=-1;prev--){
+                int not_take=prev_row[ prev+1];
+                int take=0;
+                if(prev==-1||nums[i]>nums[prev]){
+                    take=1+prev_row[ i+1];
+                }
+                cur_row[prev+1]= Math.max(take,not_take);
+            }
+            prev_row=cur_row;
+        }
+
+        return cur_row[0];
     }
     public int tabulation(int[] nums) {
         int[][]dp=new int[nums.length+1][nums.length+1];
 
         for(int i=nums.length-1;i>=0;i--){
+            //prev index is previous of curent so you take  is 1 based indexing
+            //you avoid is condition is -1 so you put +1 it could be zero
             for(int prev=i-1;prev>=-1;prev--){
-
                 int not_take=dp[ i+1][ prev+1];
                 int take=0;
                 if(prev==-1||nums[i]>nums[prev]){
