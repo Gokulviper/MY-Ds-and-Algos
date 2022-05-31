48e5debb5c05930b8eff6eb37263dc9b0e3470c4
Gokulviper <deangokul57@gmail.com>
2022-05-31 18:42:23 +0530

    updated

--- src/ArrayProblems/TrappingRainWater.java
+++ src/ArrayProblems/TrappingRainWater.java
@@ -4,6 +4,30 @@ class TrappingRainWater {
     public static void main(String[] args) {
 
     }
+    public int trapOptimized(int[] height) {
+        int n = height.length;
+        int left = 0, right = n - 1;
+        int res = 0;
+        int maxLeft = 0, maxRight = 0;
+        while (left <right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    res += maxLeft - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    res += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+        return res;
+    }
 
     public int trap(int[] nums) {
         //dp approach
