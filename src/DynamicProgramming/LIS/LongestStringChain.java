295382f8b932cc955a264726bdd0c65028c5f332
Gokulviper <deangokul57@gmail.com>
2022-06-01 14:41:56 +0530

    updated

--- src/DynamicProgramming/LIS/LongestStringChain.java
+++ src/DynamicProgramming/LIS/LongestStringChain.java
@@ -5,6 +5,26 @@ import java.util.Arrays;
 same pattern of LIS
  */
 class LongestStringChain {
+    public static void main(String[] args) {
+        int[]nums={1,3,5,9,10};
+        System.out.println(bina(nums,8));
+    }
+
+    private static int bina(int[] nums,int target) {
+        int start=0,end=nums.length-1;
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]<target){
+                start=mid+1;
+            }else {
+                end=mid-1;
+            }
+        }
+        return end;
+    }
+
     public int longestStrChain(String[] words) {
 
         Arrays.sort(words, (a, b)->(a.length() - b.length()));
