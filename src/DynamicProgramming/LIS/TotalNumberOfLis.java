c6320e2760838407af994f55fa2ddcbe1777bff0
Gokulviper <deangokul57@gmail.com>
2022-06-02 11:16:41 +0530

    updated

--- src/DynamicProgramming/LIS/TotalNumberOfLis.java
+++ src/DynamicProgramming/LIS/TotalNumberOfLis.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 class TotalNumberOfLis {
     public int findNumberOfLIS(int[] nums) {
           int[]dp=new int[nums.length];
-        int count[]=new int[nums.length];
+        int count[]=new int[nums.length];//count is make how many differnt subsequnce
+        ///if you see any equal dp[i]==dp[j]+1 index you can add up the current count you can return count
+        //the max count how many max count in the array add then your answer
           Arrays.fill(count,1);
         Arrays.fill(dp,1);//beacuse the lowest increasing subseqence length is 1
 int max=1;
