9c69bf1dd607be6173e3b3b02a00f4106bce07ec
Gokulviper <deangokul57@gmail.com>
2022-06-02 14:46:29 +0530

    updated

--- src/DynamicProgramming/Array/HouseRobber2.java
+++ src/DynamicProgramming/Array/HouseRobber2.java
@@ -3,6 +3,29 @@ package DynamicProgramming.Array;
 import java.util.Arrays;
 
 class HouseRobber2 {
+    public int tabulation(int[] nums) {
+        if(nums.length==1)return nums[0];
+
+        int[]num1=  Arrays.copyOfRange(nums,0,nums.length-2);
+        int[]num2=  Arrays.copyOfRange(nums,1,nums.length-1);
+        int[]dp=new int[nums.length-1];
+        int[]dp1=new int[nums.length-1];
+        dp[0]=nums[0];
+        dp1[0]=nums[1];
+        for(int i=1;i<num1.length;i++){
+            int npick=dp[i-1];
+            int pick=0;
+            if(i>1)pick=num1[i]+dp[i-2];
+            dp[i]=Math.max(pick,npick);
+        }
+        for(int i=1;i<num2.length;i++){
+            int npick=dp1[i-1];
+            int pick=0;
+            if(i>1)pick=num2[i]+dp1[i-2];
+            dp1[i]=Math.max(pick,npick);
+        }
+        return Math.max(dp1[num1.length-1],dp[num2.length-1]);
+    }
     public int rob(int[] nums) {
         if(nums.length==1)return nums[0];
     int[]num1=new int[nums.length-1];
