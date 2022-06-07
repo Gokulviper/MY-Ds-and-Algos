8e8415702478b6fe02ae34595756171b1799a426
Gokulviper <deangokul57@gmail.com>
2022-06-07 13:02:34 +0530

    updated

--- src/ArrayProblems/Greedy/JumpGame1.java
+++ src/ArrayProblems/Greedy/JumpGame1.java
@@ -1,7 +1,23 @@
 package ArrayProblems.Greedy;
 
 class JumpGame1 {
-    public boolean canJump(int[] nums) {
+    public static void main(String[] args) {
+        int[]nums={2,3,1,1,4};
+        boolean ans=f(nums,1,0);
+        System.out.println(ans);
+
+    }
+
+    private static boolean f(int[] nums, int i,int max) {
+        if (i>=nums.length-1)return true;
+        if (i!=1){
+            max=Math.max(max,i+nums[i]);
+            return max <i+1?false:true;
+        }
+         return     f(nums, i+nums[i], max);
+    }
+
+    public static boolean canJump(int[] nums) {
       int max=0;
         for(int i=0;i<nums.length-1;i++){
             max=Math.max(max,i+nums[i]);
