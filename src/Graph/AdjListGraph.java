2167f19cb95f24141c44a0198194fcc978b8c2c8
Gokulviper <deangokul57@gmail.com>
2022-06-13 14:23:26 +0530

    updated

--- src/Graph/AdjListGraph.java
+++ src/Graph/AdjListGraph.java
@@ -4,37 +4,35 @@ import java.util.*;
 
 public class AdjListGraph {
 
-    private LinkedList<Integer>[] adj;
+    private ArrayList<ArrayList<Integer>> adj;
     private int V; // number of vertices
     private int E; // number of edges
 
     public AdjListGraph(int nodes) {
         this.V = nodes;
         this.E = 0;   //Initialy edges is 0 wd can add every edges in manual adding edges
-        this.adj = new LinkedList[nodes]; //craeted inner linked list arrays
-        for (int v = 0; v < V; v++) {
-            adj[v] = new LinkedList<>();
-        }
+        this.adj = new ArrayList<>(); //craeted inner linked list arrays
+      for (int i=0;i<V;i++){
+          adj.add(new ArrayList<>());
+      }
     }
 
     public void addEdge(int u, int v) {
-        adj[u].add(v);
-        adj[v].add(u);
+        adj.get(u).add(v);
+        adj.get(v).add(u);
         E++;
     }
 
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(V + " vertices, " + E + " edges " + "\n");
-        for (int v = 0; v < V; v++) {
-            sb.append(v + ": ");
-            for (int w : adj[v]) {
-                sb.append(w + " ");
-            }
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
+  public void print(){
+      for (int i = 0; i < V; i++) {
+          for (int j = 0; j < adj.get(i).size(); j++) {
+              System.out.print(adj.get(i).get(j)+" ");
+          }
+          System.out.println();
+      }
+
+  }
+
     public void bfs(int s) {
         boolean[] visited = new boolean[V];
 
@@ -46,7 +44,7 @@ public class AdjListGraph {
             int u = q.poll();
             System.out.print(u + " ");
 
-            for(int v : adj[u]) {
+            for(int v : adj.get(u)) {
                 if(!visited[v]) {
                     visited[v] = true;
                     q.offer(v);
@@ -65,7 +63,7 @@ public class AdjListGraph {
                 visited[u] = true;
                 System.out.print(u + " ");
 
-                for(int v : adj[u]) {
+                for(int v : adj.get(u)) {
                     if(!visited[v]) {
                         stack.push(v);
                     }
@@ -85,7 +83,7 @@ public class AdjListGraph {
     private void dfs(int v, boolean[] visited) {
         visited[v] = true;
         System.out.print(v + " ");
-        for(int w : adj[v]){
+        for(int w : adj.get(v)){
             if(!visited[w]){
                 dfs(w, visited);
             }
@@ -94,9 +92,9 @@ public class AdjListGraph {
 
     public static void main(String[] args) {
         AdjListGraph g = new AdjListGraph(7);
-        DetectCycleUsingBFS d=new DetectCycleUsingBFS();
-        DetectCycleUsingDFS dd=new DetectCycleUsingDFS();
-        BipartiteGraphBFS b=new BipartiteGraphBFS();
+      //  DetectCycleUsingBFS d=new DetectCycleUsingBFS();
+     //   DetectCycleUsingDFS dd=new DetectCycleUsingDFS();
+      //  BipartiteGraphBFS b=new BipartiteGraphBFS();
         g.addEdge(0, 1);
         g.addEdge(1, 2);
         g.addEdge(2, 3);
@@ -104,9 +102,9 @@ public class AdjListGraph {
         g.addEdge(4, 5);
         g.addEdge(4, 6);
         g.addEdge(1, 6);
+      //  System.out.println(g.toString());
 
-
-
+          g.print();
 
 
     }
