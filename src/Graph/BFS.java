a26a4874175aa75204d5fc0424fb8a8ced3307d0
Gokulviper <deangokul57@gmail.com>
2022-06-13 14:43:57 +0530

    updated

--- src/Graph/BFS.java
+++ src/Graph/BFS.java
@@ -4,55 +4,29 @@ import java.util.*;
 
 class BFS {
 
-    public static ArrayList< Integer > bfsOfGraph1(int V, ArrayList < ArrayList < Integer >> adj) {
-    boolean[]vis=new boolean[V];
-    ArrayList<Integer>ans=new ArrayList<>();
-    Queue<Integer> queue=new LinkedList<>();
-    vis[0]=true;
-    queue.add(0);
-    while (!queue.isEmpty()){
-        int u=queue.poll();
-        ans.add(u);
-        for (int v:adj.get(u)) {
-            if (!vis[v]) {
-                vis[v] = true;
-                queue.add(v);
-            } } }
-    return ans;
-    }
+
         public static ArrayList< Integer > bfsOfGraph(int V, ArrayList < ArrayList < Integer >> adj) {
 
         ArrayList < Integer > bfs = new ArrayList < > ();
         boolean vis[] = new boolean[V];
         Queue < Integer > q = new LinkedList < > ();
-
         q.add(0);
         vis[0] = true;
-
         while (!q.isEmpty()) {
-            Integer node = q.poll();
-            bfs.add(node);
+            Integer u = q.poll();
+            bfs.add(u);
 
             // Get all adjacent vertices of the dequeued vertex s
             // If a adjacent has not been visited, then mark it
             // visited and enqueue it
-            for (Integer i: adj.get(node)) {
-                if (!vis[i]) {
-                    vis[i] = true;
-                    q.add(i);
+            for (Integer v: adj.get(u)) {
+                if (!vis[v]) {
+                    vis[v] = true;
+                    q.add(v);
                 }
             }
         }
-
         return bfs;
-
-        // Code here
-    }
-
-    static void printAns(ArrayList < Integer > ans) {
-        for (int i = 0; i < ans.size(); i++) {
-            System.out.print(ans.get(i) + " ");
-        }
     }
     public static void main(String args[]) {
 
@@ -71,7 +45,7 @@ class BFS {
 
 
         ArrayList < Integer > ans = bfsOfGraph(5, adj);
-        printAns(ans);
+        System.out.println(ans);
 
 
 
