0b1bdae44c9d00017c7f31e9f3c9ee771880621a
Gokulviper <deangokul57@gmail.com>
2022-06-15 10:17:04 +0530

    updated

--- src/Graph/GraphTheories/TopoLogicalSortBFS.java
+++ src/Graph/GraphTheories/TopoLogicalSortBFS.java
@@ -2,52 +2,7 @@ package Graph.GraphTheories;
 
 import java.util.*;
 
-class Solution
-    {
-            public boolean isCyclic(int N, ArrayList<ArrayList<Integer>> adj) {
-                int topo[] = new int[N];
-                int indegree[] = new int[N];
-                
-                //finding indegree
-                for(int i = 0;i<N;i++) {
-                    for(Integer it: adj.get(i)) {
-                        indegree[it]++;
-                    }
-                }
-                
-                
-                Queue<Integer> q = new LinkedList<Integer>();
-                for(int i = 0;i<N;i++) {
-                    //adding nodes to queue with indegree = 0
-                    if(indegree[i] == 0) {
-                        q.add(i);
-                    }
-                }
-                
-                int cnt = 0;
-                int ind=0;
-                
-                while(!q.isEmpty()) {
-                    Integer node = q.poll();
-                    topo[ind++] = node;
-                    cnt++;
-                    //getting neighbour nodes of popped node and decreasing  their 
-                  //  indegree by1
-                    for(Integer it: adj.get(node)) {
-                        indegree[it]--;
-                        if(indegree[it] == 0) {
-                            q.add(it);
-                        }
-                    }
-                }
-                 //printing topological ordering of nodes
-                for (int i=0;i< topo.length;i++){
-                    System.out.print(topo[i]+" ");
-                }
-                if(cnt == N) return false;
-                return true;
-            }
-        }
+
 public class TopoLogicalSortBFS {
         public static void main(String args[]) {
 
@@ -65,9 +20,53 @@ public class TopoLogicalSortBFS {
             adj.get(4).add(1);
             adj.get(3).add(1);
             adj.get(2).add(3);
+isCyclic(6,adj);
 
 
-           new Solution().isCyclic(6,adj);
 
         }
+
+    public static boolean isCyclic(int N, ArrayList<ArrayList<Integer>> adj) {
+        int topo[] = new int[N];
+        int indegree[] = new int[N];
+
+        //finding indegree
+        for(int i = 0;i<N;i++) {
+            for(Integer v: adj.get(i)) {
+                indegree[v]++;
+            }
+        }
+
+
+        Queue<Integer> q = new LinkedList<Integer>();
+        for(int i = 0;i<N;i++) {
+            //adding nodes to queue with indegree = 0
+            if(indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        int cnt = 0;
+        int ind=0;
+
+        while(!q.isEmpty()) {
+            Integer u = q.poll();
+            topo[ind++] = u;
+            cnt++;
+            //getting neighbour nodes of popped u and decreasing  their
+          //  indegree by1
+            for(Integer v: adj.get(u)) {
+                indegree[v]--;
+                if(indegree[v] == 0) {
+                    q.add(v);
+                }
+            }
+        }
+        //printing topological ordering of nodes
+        for (int i=0;i< topo.length;i++){
+            System.out.print(topo[i]+" ");
+        }
+        if(cnt == N) return false;
+        return true;
+    }
     }
