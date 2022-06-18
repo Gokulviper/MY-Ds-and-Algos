e42bd5011968130a9d40d87416283903b3d365b1
Gokulviper <deangokul57@gmail.com>
2022-06-18 15:50:48 +0530

    updated

--- src/ArrayProblems/BinarySearch/FindMedianOfTwoSortedArrays.java
+++ src/ArrayProblems/BinarySearch/FindMedianOfTwoSortedArrays.java
@@ -8,13 +8,13 @@ class FindMedianOfTwoSortedArrays {
         }
     int low=0,high=m,medianPos=((m+n)+1)/2;
     while(low<=high) {
-        int cut1 = (low+high)>>1;
-        int cut2 = medianPos - cut1;
+        int mid1 = (low+high)>>1;
+        int mid2 = medianPos - mid1;
         
-        int l1 = (cut1 == 0)? Integer.MIN_VALUE:arr1[cut1-1];
-        int l2 = (cut2 == 0)? Integer.MIN_VALUE:arr2[cut2-1];
-        int r1 = (cut1 == m)? Integer.MAX_VALUE:arr1[cut1];
-        int r2 = (cut2 == n)? Integer.MAX_VALUE:arr2[cut2];
+        int l1 = (mid1 == 0)? Integer.MIN_VALUE:arr1[mid1-1];
+        int l2 = (mid2 == 0)? Integer.MIN_VALUE:arr2[mid2-1];
+        int r1 = (mid1 == m)? Integer.MAX_VALUE:arr1[mid1];
+        int r2 = (mid2 == n)? Integer.MAX_VALUE:arr2[mid2];
         
         if(l1<=r2 && l2<=r1) {
             if((m+n)%2 != 0)
@@ -22,8 +22,8 @@ class FindMedianOfTwoSortedArrays {
             else 
                 return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
         }
-        else if(l1>r2) high = cut1-1;
-        else low = cut1+1;
+        else if(l1>r2) high = mid1-1;
+        else low = mid1+1;
     }
     return 0.0;
     }
