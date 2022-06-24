72e566d32d77df476a379b7bfc31ad1a3ded0ff9
Gokulviper <deangokul57@gmail.com>
2022-06-24 11:37:09 +0530

    updated

---
+++ src/Treee/problems/DiameterOfBinaryTree.java
@@ -0,0 +1,18 @@
+package Treee.problems;
+
+class DiameterOfBinaryTree {
+  
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[]nums=new int[1];
+         f(root,nums);
+        return nums[0];
+    }
+    int f(TreeNode root,int[]nums){
+        if(root==null)return 0;
+     int lh=f(root.left,nums);
+        int rh=f(root.right,nums);
+        nums[0]=Math.max(lh+rh,nums[0]);
+        
+        return 1+Math.max(lh,rh);
+    }
+}
