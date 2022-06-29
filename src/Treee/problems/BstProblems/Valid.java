3b393dea8f1f814759c6c054b961487ee87c51bc
Gokulviper <deangokul57@gmail.com>
2022-06-29 10:24:00 +0530

    updated

--- src/Treee/problems/BstProblems/Valid.java
+++ src/Treee/problems/BstProblems/Valid.java
@@ -1,16 +1,16 @@
 package Treee.problems.BstProblems;
 
-class Valid {
-public boolean isValidBST(TreeNode root) {
-return valid(root,Long.MIN_VALUE,Long.MAX_VALUE);
-}
-boolean valid(TreeNode root,Long min,Long max){
-if(root==null){
-return true;
-}
-if(root.val<=min||root.val>=max){
-return false;
-}
-return valid(root.left,min,(long)root.val)&&valid(root.right,(long)root.val,max);
-}
-}
+    class Valid {
+    public boolean isValidBST(TreeNode root) {
+    return valid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    boolean valid(TreeNode root,Long min,Long max){
+    if(root==null){
+    return true;
+    }
+        if(root.val<=min||root.val>=max){
+    return false;
+    }
+    return valid(root.left,min,(long)root.val)&&valid(root.right,(long)root.val,max);
+    }
+    }
