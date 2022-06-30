63a919ab1fb5962876804f903a6e8f2d82e24ac8
Gokulviper <deangokul57@gmail.com>
2022-06-30 17:55:14 +0530

    updated

--- src/String/SlidingWindow/SubStringWithConcatenationOfSubstrings.java
+++ src/String/SlidingWindow/SubStringWithConcatenationOfSubstrings.java
@@ -2,10 +2,10 @@ package String.SlidingWindow;
 
 import java.util.*;
 
-class Solution {
+class SubStringWithConcatenationOfSubstrings {
     public static void main(String[]a){
-        String s = "barfoothefoobarman";
-        String[]wors={"foo","bar"};
+        String s = "barfoofoobarthefoobarman";
+        String[]wors={"bar","foo","the"};
         System.out.println(findSubstring(s,wors));
     }
     public static List<Integer> findSubstring(String s1, String[] words) {
@@ -16,22 +16,29 @@ class Solution {
           s2+=ss;
       }
         List<Integer> ans=new ArrayList();
+      Set<Integer>set=new HashSet<>();
         for(int i=0;i<s2.length();i++){
-            hash1[s1.charAt(i)-'a']++;
-            hash2[s2.charAt(i)-'a']++;
+            hash1[s2.charAt(i)-'a']++;
+            hash2[s1.charAt(i)-'a']++;
+        }
+        int len=words[0].length();
+        if(match(hash1,hash2)){
+            ans.add(0);
         }
         int end=s2.length(),start=0;
-    for(;end<s1.length()-1;end++){
+    while(end<s1.length()){
+
+        int num=0;
+        while (num<len) {
+            hash2[s1.charAt(end++) - 'a']++;
+            hash2[s1.charAt(start++) - 'a']--;
+            num++;
+        }
         if(match(hash1,hash2)){
             ans.add(start);
         }
-        hash2[s2.charAt(end)-'a']++;
-        hash2[s2.charAt(start++)-'a']--;
-        
     }
-        if(match(hash1,hash2)){
-            ans.add(start);
-        }
+
         return ans;
     }
     private static boolean match(int[]hash1,int[]hash2){
