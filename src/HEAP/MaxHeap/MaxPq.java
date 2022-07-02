c200f2ab6a2657359834bb0f76288fe027c0d87e
Gokulviper <deangokul57@gmail.com>
2022-07-02 11:19:51 +0530

    updated

--- src/HEAP/MaxHeap/MaxPq.java
+++ src/HEAP/MaxHeap/MaxPq.java
@@ -1,5 +1,8 @@
 package HEAP.MaxHeap;
 
+
+import java.util.Arrays;
+
 public class MaxPq {
     int heap[];//store the max heap
     int n; //this n holdes as index maintainer and the size giver
@@ -12,6 +15,49 @@ public class MaxPq {
     }
     public boolean isEmpty(){
         return n==0;
+    }
+    public void insert(int value){
+        if (n==heap.length-1){
+            resizeHeap(2*heap.length);
+        }
+        n++;
+        heap[n]=value;
+        swim(n);
+    }
+
+    private void swim(int n) {
+        //n/2 is check the parent is in the correct position or not
+        while (n>1&&heap[n/2]<heap[n]){
+            Swap(n/2,n);
+            n/=2;
+        }
+    }
+
+    private void Swap(int a, int b) {
+        int t=heap[a];
+        heap[a]=heap[b];
+        heap[b]=t;
+    }
+
+    private void resizeHeap(int size) {
+        int []temp=new int[size];
+        for (int i = 0; i <heap.length ; i++) {
+            temp[i]=heap[i];
+        }
+        heap=temp;
+    }
+
+    public static void main(String[] args) {
+        MaxPq pq=new MaxPq(10);
+     pq.insert(2);
+        pq.insert(5);
+        pq.insert(8);
+        pq.insert(3);
+        pq.insert(9);
+        pq.insert(21);
+        pq.insert(1);
+        System.out.println(pq.size());
+        System.out.println(Arrays.toString(pq.heap));
     }
 
 }
