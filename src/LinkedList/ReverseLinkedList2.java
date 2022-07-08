9b200d15966e395483e83ad40bef92604f4694e9
Gokulviper <deangokul57@gmail.com>
2022-07-08 14:42:44 +0530

    updated

---
+++ src/LinkedList/ReverseLinkedList2.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+class ReverseLinkedList2 {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+    ListNode current  = head;
+        ListNode prev = null;
+        for(int i=0;current!=null && i<left-1;i++){
+            prev = current;
+            current = current.next;
+        }
+        ListNode last = prev;
+        ListNode newEnd = current;
+        ListNode next = current.next;
+        for(int i=0; current != null && i<right-left+1;i++){
+            current.next = prev;
+            prev = current;
+            current = next;
+            if(next!=null){
+                next = next.next;
+            }
+        }
+        if(last!=null){
+            last.next = prev;
+        }else{
+            head = prev;
+        }
+        newEnd.next = current;
+        return head;
+    }
+}
