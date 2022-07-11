bb3fe05f10e339ae2fe95bd170340747c938100d
Gokulviper <deangokul57@gmail.com>
2022-07-11 18:15:14 +0530

    updated

---
+++ src/LinkedList/Solution.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+import java.util.HashMap;
+
+class Node{
+    Node next;
+    int val;
+    Node random;
+    Node(int val){
+        this.val=val;
+    }
+}
+class Solution {
+    public Node copyRandomList(Node head) {
+        HashMap<Node,Node> map=new HashMap();
+        Node cur=head;
+        while(cur!=null){
+            map.put(cur,new Node(cur.val));
+            cur=cur.next;
+        }
+        cur=head;
+        while(cur!=null){
+            map.get(cur).next=map.get(cur.next);
+            map.get(cur).random=map.get(cur.random);
+            cur=cur.next;
+        }
+       return map.get(head);
+    }
+}
