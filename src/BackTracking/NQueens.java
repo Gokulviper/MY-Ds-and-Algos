62047a84719392cac444f602766a4f61eed9fd8d
Gokulviper <deangokul57@gmail.com>
2022-07-19 17:56:51 +0530

    updated

--- src/BackTracking/NQueens.java
+++ src/BackTracking/NQueens.java
@@ -7,71 +7,44 @@ class NQueens {
         System.out.println(solveNQueens(4));
     }
     public static List<List<String>> solveNQueens(int n) {
-        List<List<String>> result = new ArrayList<>();
-        // Create n*n board and fill it with dots "."
-        char[][] board = new char[n][n];
-
-        for(char[] row : board) {
-            Arrays.fill(row, '.');
+        List<List<String>> ans=new ArrayList();
+        char[][]nums=new char[n][n];
+        for(char[]row:nums){
+            Arrays.fill(row,'.');
         }
-
-        // Call recursive function for 0th row
-        solveNQueens(board, n, 0, result);
-
-        return result;
+        solve(n,nums,0,ans);
+        return ans;
     }
-
-    private static void solveNQueens(char[][] board, int n, int row, List<List<String>> result) {
-        /* If all the queens have been placed in current path,
-        put the formatted result into list and backtrack. */
-        if( row == n ) {
-            result.add( formatResult( board ) );
+    public static void solve(int n,char[][]nums,int row, List<List<String>> ans){
+        if(row==n){
+            ans.add(new ArrayList(format(nums)));
             return;
         }
-
-        /* Try to place the queen in each column of the current row */
-        for(int col=0; col<n; col++) {
-
-            // Check current queen is safe at current column in the row
-            if( isSafe( board, n, row, col ) ) {
-
-                // Place the queen at current column
-                board[row][col] = 'Q';
-
-                // Try to place the next queen in the row
-                solveNQueens(board, n, row+1, result);
-
-                /* Remove the queen after the current combination has been
-                tried */
-                board[row][col] = '.';
+        for(int col=0;col<n;col++){
+            if(isSafe(n,row,col,nums)){
+                nums[row][col]='Q';
+                solve(n,nums,row+1,ans);
+                nums[row][col]='.';
             }
         }
     }
-    private static boolean isSafe(char[][] board, int n, int row, int col) {
-
-
-        for (int i = 0; i < row; i++) {
-            if (board[i][col] == 'Q')
-                return false;
+    private static boolean isSafe(int n,int row,int col,char[][]nums){
+        for(int i=0;i<n;i++){
+            if(nums[i][col]=='Q')return false;
         }
-
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q')
-                return false;
+        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+            if(nums[i][j]=='Q')return false;
         }
-
-        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-            if (board[i][j] == 'Q')
-                return false;
+        for(int i=row-1,j=col+1;i>=0&&j<n;i--,j++){
+            if(nums[i][j]=='Q')return false;
         }
-
         return true;
     }
-    private static List<String> formatResult(char[][] board) {
-        List<String> result = new ArrayList<>();
-        for(char[] row : board) {
-            result.add( new String( row ) );
+    private static List<String> format(char[][]nums){
+        List<String> ans=new ArrayList();
+        for(char[]row:nums){
+            ans.add(new String(row));
         }
-        return result;
+        return ans;
     }
 }
--- src/BackTracking/Nqueens2.java
+++ src/BackTracking/Nqueens2.java
@@ -1,7 +1,64 @@
 package BackTracking;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Nqueens2 {
+    int count = 0;
+
+    public static void main(String[] args) {
+        Nqueens2 n = new Nqueens2();
+        System.out.println(n.totalNQueens(2));
+    }
+
     public int totalNQueens(int n) {
-        return 0;
+
+        // Create n*n board and fill it with dots "."
+        char[][] board = new char[n][n];
+
+        for (char[] row : board) {
+            Arrays.fill(row, '.');
+        }
+
+        // Call recursive function for 0th row
+         solveNQueens(board, n, 0);
+
+      return count;
+    }
+
+    private void solveNQueens(char[][] board, int n, int row ) {
+        if(row==n){
+            count++;
+            return;
+        }
+        for (int col = 0; col <n ; col++) {
+
+
+        if (isSafe(board,row,col,n)) {
+            board[row][col] = 'Q';
+            solveNQueens(board, n, row + 1);
+            board[row][col] = '.';
+        }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col, int n) {
+        for (int i = 0; i <n ; i++) {
+            if (board[i][col]=='Q'){
+                return false;
+            }
+        }
+        for (int i = row-1,j=col-1; i >=0&&j>=0 ; i--,j--) {
+            if (board[i][j]=='Q'){
+                return false;
+            }
+        }
+        for (int i = row-1,j=col+1; i >=0&&j<n ; i--,j++) {
+            if (board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
     }
 }
