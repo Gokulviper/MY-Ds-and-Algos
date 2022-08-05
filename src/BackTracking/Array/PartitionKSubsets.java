7903efcef967559da54b204b56ff2588d7d25161
Gokulviper <deangokul57@gmail.com>
2022-08-05 19:21:14 +0530

    updated

--- src/BackTracking/Array/PartitionKSubsets.java
+++ src/BackTracking/Array/PartitionKSubsets.java
@@ -1,7 +1,12 @@
 package BackTracking.Array;
 
 class PartitionKSubsets {
-    public boolean canPartitionKSubsets(int[] nums, int k) {
+    public static void main(String[] args) {
+        int[]nums={4,3,2,3,5,2,1};
+        int k=2;
+        System.out.println(canPartitionKSubsets(nums,4));
+    }
+    public static boolean canPartitionKSubsets(int[] nums, int k) {
         int sum=0;
         for(int num:nums)sum+=num;
         if(sum%k!=0)return false;
@@ -9,7 +14,12 @@ class PartitionKSubsets {
         boolean[]vis=new boolean[nums.length];
         return f(nums,k,sum,0,0,vis);
     }
-    boolean f(int[]nums,int k,int target,int sum,int start,boolean[]vis){
+    /*
+    flow of this loop bases recursion
+    if you get enter into the number checks the fully run entre loop
+    for finding next element-
+     */
+   static boolean f(int[]nums,int k,int target,int sum,int start,boolean[]vis){
         if(k==1)return true;
         if(sum>target)return false;
         if(sum==target){
