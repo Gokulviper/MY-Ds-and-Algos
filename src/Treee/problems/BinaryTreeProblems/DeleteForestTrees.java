4e5f7efeb53a8fa62e58001a962500f30a79460b
Gokulviper <deangokul57@gmail.com>
2022-08-16 16:56:28 +0530

    updated

---
+++ src/Treee/problems/BinaryTreeProblems/DeleteForestTrees.java
@@ -0,0 +1,35 @@
+package Treee.problems.BinaryTreeProblems;
+
+import java.util.*;
+
+class DeleteForestTrees {
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+       List<TreeNode> forest = new ArrayList<>();
+        if (root == null) return forest;
+        Set<Integer> set = new HashSet<>();
+        for(int i : to_delete) {
+            set.add(i);
+        }
+        deleteNodes(root, set, forest);
+        if (!set.contains(root.val)) {
+            forest.add(root);
+        }
+        
+        return forest;
+    }
+
+    private TreeNode deleteNodes(TreeNode node, Set<Integer> set, List<TreeNode> forest) {
+        if (node == null) return null;
+
+        node.left = deleteNodes(node.left, set, forest);
+        node.right = deleteNodes(node.right, set, forest);
+
+        if (set.contains(node.val)) {
+            if (node.left != null) forest.add(node.left);
+            if (node.right != null) forest.add(node.right);
+            return null;
+        }
+
+        return node;
+    }
+}
