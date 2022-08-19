91a407316b29bba6d3127924699cc0b543ec20f9
Gokulviper <deangokul57@gmail.com>
2022-08-19 17:28:54 +0530

    updated

--- src/LinkedList/MergeKSortedLists.java
+++ src/LinkedList/MergeKSortedLists.java
@@ -2,7 +2,29 @@ package LinkedList;
 
 import HEAP.MinHeap.MinPQ;
 
+import java.util.PriorityQueue;
+
 class MergeKSortedLists {
+    public ListNode mergeKLists1(ListNode[] lists) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->a - b);
+        ListNode list=new ListNode(-1);
+        ListNode cur=list;
+        for(ListNode l:lists){
+            ListNode n=l;
+            while(n!=null){
+
+                pq.add(n.data);
+                n=n.next;
+            }
+        }
+        while(!pq.isEmpty()){
+            int num=pq.poll();
+            ListNode node=new ListNode(num);
+            cur.next=node;
+            cur=cur.next;
+        }
+        return list.next;
+    }
     public ListNode mergeKLists(ListNode[] lists) {
         MinPQ pq=new MinPQ(10);
         int len=0;
