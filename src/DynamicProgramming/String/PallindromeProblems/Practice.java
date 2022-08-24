f52c18ad98ebda6ebb8f7c4a63707a4c98f7ebdf
Gokulviper <deangokul57@gmail.com>
2022-08-24 20:12:25 +0530

    updated

--- src/DynamicProgramming/String/PallindromeProblems/Practice.java
+++ src/DynamicProgramming/String/PallindromeProblems/Practice.java
@@ -4,26 +4,51 @@ package DynamicProgramming.String.PallindromeProblems;
 
 //import static sun.net.ftp.FtpReplyCode.find;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Practice {
     public static void main(String[] args) {
-        String s="";
-        findstring(s);
+   funstr("abbbbbbcccsdyh");
     }
+
+    private static void funstr(String s) {
+       char ch=s.charAt(0);
+       int num=0;
+       String ans="";
+       int i=0;
+       while (i<s.length()){
+           ch=s.charAt(i++);
+           num=1;
+           while (i<s.length()&&ch==s.charAt(i)){
+               num++;
+               i++;
+           }
+           ans+=ch+""+num;
+
+       }
+        System.out.println(ans);
+   
+    }
+
+    private static void fun1(int[] nums,int target) {
+        for (int i = 0; i <nums.length ; i++) {
+            for (int j = i; j <=nums.length ; j++) {
+                for (int k = i; k <j ; k++) {
+                    System.out.print(nums[k]);
+                }
+                System.out.println();
+            }
+
+        }
+        }
+
 
     private static void findstring(String s) {
 
     }
 
     static void find(int n) {
-        int ans=0;
-        int sum=0;
-        while (n>0){
-            int mod=n%10;
-            sum+=mod;
-            ans=ans*10+mod;
-            n/=10;
-        }
-        System.out.println(ans);
-        System.out.println(sum);
+        
     }
 }
