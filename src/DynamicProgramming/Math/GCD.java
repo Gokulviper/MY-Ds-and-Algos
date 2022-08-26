940f77448553e46677809aa27554c7bcddbee754
Gokulviper <deangokul57@gmail.com>
2022-08-26 19:08:40 +0530

    updated

---
+++ src/DynamicProgramming/Math/GCD.java
@@ -0,0 +1,11 @@
+package DynamicProgramming.Math;
+
+public class GCD {
+    public static void main(String[] args) {
+        gcd(24,8);
+    }
+
+    private static void gcd(int a, int b) {
+
+    }
+}
--- src/DynamicProgramming/String/PallindromeProblems/Practice.java
+++ src/DynamicProgramming/String/PallindromeProblems/Practice.java
@@ -4,40 +4,36 @@ package DynamicProgramming.String.PallindromeProblems;
 
 //import static sun.net.ftp.FtpReplyCode.find;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Practice {
     public static void main(String[] args) {
-   funstr("abbbbbbcccsdyh");
+ int[]nums={2,3,0,0,2,3,5,6,23,56,77,211};
+  fun1(nums,4);
+        System.out.println(Arrays.toString(nums));
     }
 
     private static void funstr(String s) {
-       char ch=s.charAt(0);
-       int num=0;
-       String ans="";
-       int i=0;
-       while (i<s.length()){
-           ch=s.charAt(i++);
-           num=1;
-           while (i<s.length()&&ch==s.charAt(i)){
-               num++;
-               i++;
-           }
-           ans+=ch+""+num;
-
-       }
-        System.out.println(ans);
+        for (int i = 0; i <s.length() ; i++) {
+            for (int j = i+1; j <=s.length() ; j++) {
+                System.out.println(s.substring(i,j));
+            }
+        }
    
     }
 
     private static void fun1(int[] nums,int target) {
-        for (int i = 0; i <nums.length ; i++) {
-            for (int j = i; j <=nums.length ; j++) {
-                for (int k = i; k <j ; k++) {
-                    System.out.print(nums[k]);
-                }
-                System.out.println();
+         int i=0;
+        for (int j = 0; j <nums.length ; j++) {
+            if (nums[i]%2==0&&nums[j]%2!=0){
+                int t=nums[i];
+                nums[i]=nums[j];
+                nums[j]=t;
+                i++;
+            }else if(nums[i]%2!=0){
+                i++;
             }
 
         }
@@ -49,6 +45,30 @@ public class Practice {
     }
 
     static void find(int n) {
-        
+        int outerSpace=n-1;
+        int starts=1;
+        for (int i = 0; i <=n; i++) {
+            if(i<n/2) {
+                for (int j = 0; j < outerSpace; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < starts; j++) {
+                    System.out.print("* ");
+                }
+                outerSpace--;
+                starts++;
+
+            }else{
+                for (int j = 0; j <outerSpace ; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j <starts ; j++) {
+                    System.out.print("* ");
+                }
+                outerSpace++;
+                starts--;
+            }
+            System.out.println();
+        }
     }
 }
