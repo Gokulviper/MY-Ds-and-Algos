e1dde9fa1e9a3b8973dc05f1ad4a443f25853b4c
Gokulviper <deangokul57@gmail.com>
2022-09-02 12:24:06 +0530

    updated

--- src/PatternQuestions/LookandSat.java
+++ src/PatternQuestions/LookandSat.java
@@ -2,7 +2,32 @@ package PatternQuestions;
 
 public class LookandSat {
     public static void main(String[] args) {
-    lns(5);
+    lns1(5);
+    }
+
+    private static void lns1(int n) {
+        System.out.println(1);
+        int i=1;
+        String s="1";
+        while (i<n){
+            String newS="";
+            char ch=s.charAt(0);
+            int count=1;
+            for (int j = 1; j <s.length() ; j++) {
+                if (s.charAt(j)!=s.charAt(j-1)){
+                    System.out.print(count+""+ch);
+                    newS+=""+count+""+ch;
+                    count=1;
+                    ch=s.charAt(j);
+                }else{
+                    count++;
+                }
+            }
+            System.out.println(count+""+ch);
+            newS+=""+count+""+ch;
+            s=newS;
+            i++;
+        }
     }
 
     private static void lns(int n) {
