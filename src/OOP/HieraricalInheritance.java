8da4e17005aca140bd517e3cb47cd9eb46ef09f0
Gokulviper <deangokul57@gmail.com>
2022-09-06 17:35:42 +0530

    updated

---
+++ src/OOP/HieraricalInheritance.java
@@ -0,0 +1,39 @@
+package OOP;
+class h{
+   static int a=10;
+    int b=20;
+    int c;
+    h(int c){
+        this.c=a+this.b+c;
+    }
+    public void samll(){
+        System.out.println("first  class");
+    }
+}
+class sub1 extends h{
+    sub1(int c) {
+        super(c);
+    }
+
+
+    public void sl(){
+        System.out.println("second  class");
+    }
+}
+class sub2 extends h{
+    sub2(int c) {
+        super(c);
+    }
+
+    public void sab(){
+        System.out.println("third  class");
+    }
+}
+public class HieraricalInheritance {
+    public static void main(String[] args) {
+        sub2 s=new sub2(67);
+        sub1 s1=new sub1(67);
+        System.out.println(s1.c);
+
+    }
+}
