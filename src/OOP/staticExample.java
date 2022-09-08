f878af73a0c14386fa224dfff88043443e9021ce
Gokulviper <deangokul57@gmail.com>
2022-09-08 15:06:36 +0530

    updated

---
+++ src/OOP/staticExample.java
@@ -0,0 +1,20 @@
+package OOP;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class staticExample {
+    static int a=10;
+    static void aa(){
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+   try{
+           int a=sc.nextInt();
+     }catch (Exception e){
+         System.out.println("number format exception");
+     }
+    }
+}
