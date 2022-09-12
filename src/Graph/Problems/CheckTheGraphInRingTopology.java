09a8d0fdfce5ad4c47ab2f56d3764a9880571c83
Gokulviper <deangokul57@gmail.com>
2022-09-12 16:59:12 +0530

    updated

---
+++ src/Graph/Problems/CheckTheGraphInRingTopology.java
@@ -0,0 +1,14 @@
+package Graph.Problems;
+/*
+A graph of V vertices represents a Ring topology
+if it satisfies the following three conditions:
+
+1)Number of vertices >= 3.
+2)All vertices should have degree 2.
+3)No of edges = No of Vertices.
+ */
+public class CheckTheGraphInRingTopology {
+    public static void main(String[] args) {
+
+    }
+}
