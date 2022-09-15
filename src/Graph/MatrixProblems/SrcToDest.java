8885b8808922d9f44113a527b38d8f1e897c24ca
Gokulviper <deangokul57@gmail.com>
2022-09-15 16:52:22 +0530

    updated

--- src/Graph/MatrixProblems/SrcToDest.java
+++ src/Graph/MatrixProblems/SrcToDest.java
@@ -1,39 +1,57 @@
 package Graph.MatrixProblems;
 
 
-import java.util.LinkedList;
-import java.util.Queue;
-
-class Node{
-    int row;
-    int col;
-    Node(int row,int col){
-        this.row=row;
-        this.col=col;
-    }
-}
 public class SrcToDest {
+   static String ans="";
     public static void main(String[] args) {
-      int[][]mat=new int[6][6];
-      mat[2][3]=1;
-      mat[5][2]=2;
-        System.out.println(find(mat,2,3));
+        int[][] mat = new int[6][6];
+        int i=5;
+        int j=1;
+        int row=0;
+        int col=4;
+        mat[i][j] = 1;
+        mat[row][col] = 2;
+        System.out.println(findUsingBactracking(mat, i, j, row, col));
+        System.out.println(ans);
     }
 
-    private static int find(int[][] mat, int row, int col) {
-        Queue<Node>q=new LinkedList<>();
-        q.add(new Node(row,col));
-        int level=0;
-        while (!q.isEmpty()){
-            int size=q.size();
-            while (size-->0) {
-                Node node = q.poll();
-                int i=node.row;
-                int j=node.col;
+    private static int findUsingBactracking(int[][] mat, int i, int j, int r, int c) {
+        int distance = Integer.MAX_VALUE;
+        boolean[][] vis = new boolean[mat.length][mat[0].length];
+        return findUsingBactrackingutil(mat, i, j, r, c, vis, distance, 0,"");
 
+    }
+
+    private static int findUsingBactrackingutil(int[][] mat, int i, int j, int r, int c, boolean[][] vis, int minDistacne, int distance,String path)
+        {
+            if (i == r && j == c) {
+                if (minDistacne>distance){
+                    ans=path;
+                }
+                minDistacne = Math.min(minDistacne, distance);
+                return minDistacne;
             }
+            vis[i][j] = true;
+            if (isSafe(mat, vis, i + 1, j)) {
+                minDistacne = Math.min(minDistacne, findUsingBactrackingutil(mat, i + 1, j, r, c, vis, minDistacne, distance+1,path+"("+i+","+j+")->"));
+            }
+            if (isSafe(mat, vis, i - 1, j)) {
+                minDistacne = Math.min(minDistacne, findUsingBactrackingutil(mat, i - 1, j, r, c, vis, minDistacne, distance+1,path+"("+i+","+j+")->"));
+            }
+            if (isSafe(mat, vis, i, j - 1)) {
+                minDistacne = Math.min(minDistacne, findUsingBactrackingutil(mat, i, j - 1, r, c, vis, minDistacne, distance+1,path+"("+i+","+j+")->"));
+            }
+            if (isSafe(mat, vis, i, j + 1)) {
+                minDistacne = Math.min(minDistacne, findUsingBactrackingutil(mat, i, j + 1, r, c, vis, minDistacne, distance+1,path+"("+i+","+j+")->"));
+            }
+            vis[i][j]=false;
+            return minDistacne;
         }
-        return 0;
+
+
+        private static boolean isSafe ( int[][] mat, boolean[][] vis, int i, int j){
+            return i >= 0 && j >= 0 && i < mat.length && j < mat[0].length && !vis[i][j];
+        }
 
     }
-}
+
---
+++ src/Graph/MatrixProblems/SrcToDisUsingBfs.java
@@ -0,0 +1,56 @@
+package Graph.MatrixProblems;
+
+import ArrayProblems.Sorting.GeneralAlgorithms.P;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Point{
+    int row,col;
+    Point(int r,int c){
+        row=r;
+        col=c;
+    }
+}
+class QueueNode{
+    Point p;
+    int dis;
+    QueueNode(Point p,int d){
+        this.p=p;
+        dis=d;
+    }
+}
+public class SrcToDisUsingBfs {
+    public static void main(String[] args) {
+        int[][] mat = new int[6][6];
+        int i=5;
+        int j=1;
+        int row=0;
+        int col=4;
+        mat[i][j] = 1;
+        mat[row][col] = 2;
+        System.out.println( findShortestPath(mat,i,j,row,col));  ;
+    }
+
+    private static int findShortestPath(int[][] mat, int i, int j, int row, int col) {
+        Queue<QueueNode>q=new LinkedList<>();
+        boolean[][]vis=new boolean[mat.length][mat[0].length];
+        vis[i][j]=true;
+        q.add(new QueueNode(new Point(i,j),0));
+        int[][]dir=new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
+        while (!q.isEmpty()){
+            QueueNode qNode=q.poll();
+            Point src=qNode.p;
+            if (src.row==row&&src.col==col)return qNode.dis;
+            for (int k = 0; k <4 ; k++) {
+                int r=src.row+dir[i][0];
+                int c=src.col+dir[0][j];
+                if(r>=0&&c>=0&&r<mat.length&&c<mat[0].length&&!vis[r][c]){
+                    vis[r][c]=true;
+                    q.add(new QueueNode(new Point(r,c), qNode.dis+1));
+                }
+            }
+        }
+        return -1;
+    }
+}
