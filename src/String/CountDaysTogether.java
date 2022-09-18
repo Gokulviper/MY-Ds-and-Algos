54f34754aab0ab2bb90fe8fcb2412e508908ba87
Gokulviper <deangokul57@gmail.com>
2022-09-18 09:37:34 +0530

    updated

--- src/String/CountDaysTogether.java
+++ src/String/CountDaysTogether.java
@@ -2,9 +2,45 @@ package String;
 
 class CountDaysTogether {
     public static void main(String[] args) {
-        String s="01";
-        int a=Integer.parseInt(s);
-        System.out.println(a+4);
+       fun(3);
+    }
+
+    private static void fun(int n) {
+        int size=2*n-1;
+        int frontStars=2;
+        int endStars=2;
+        int numberLoopSize=1;
+        for (int i = 0; i <size ; i++) {
+            int k=0;
+           ;
+            for ( int j = 0; j <frontStars ; j++) {
+                System.out.print("*");
+                k++;
+            }
+            int number=n;
+            for (int j = 0; j <numberLoopSize ; j++) {
+                if (k<n/2+1){
+                    System.out.print(number--);
+                }else{
+                    System.out.print(number++);
+                }
+                k++;
+            }
+            for (int j = 0; j <endStars ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+           if (i<n/2+1){
+               frontStars--;
+               endStars--;
+               numberLoopSize+=2;
+           }else{
+               frontStars++;
+               endStars++;
+               numberLoopSize-=2;
+           }
+            System.out.println();
+        }
     }
 
 }
