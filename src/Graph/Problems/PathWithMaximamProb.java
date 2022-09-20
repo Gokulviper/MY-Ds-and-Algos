89ac2035a005f344c5ceb38d85a25e55619ca645
Gokulviper <deangokul57@gmail.com>
2022-09-20 09:58:29 +0530

    updated

--- src/Graph/Problems/PathWithMaximamProb.java
+++ src/Graph/Problems/PathWithMaximamProb.java
@@ -2,41 +2,58 @@ package Graph.Problems;
 
 import java.util.*;
 class pair{
-    int key;double value;
-    pair(int k,double v){
-        key=k;
-        value=v;
+    int key;
+    double value;
+
+    public pair(int key,double value){
+        this.key = key;
+        this.value = value;
     }
+
+    public int getKey(){return key;}
+    public double getValue(){return value;}
 }
 class PathWithMaximamProb {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
-        List<List<pair>> adj=new ArrayList();
-         for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        ArrayList<ArrayList<pair>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+
         for(int i=0;i<edges.length;i++){
             adj.get(edges[i][0]).add(new pair(edges[i][1],succProb[i]));
-              adj.get(edges[i][1]).add(new pair(edges[i][0],succProb[i]));
+            adj.get(edges[i][1]).add(new pair(edges[i][0],succProb[i]));
         }
-    double prob[]=new double[n];
+
+        double prob[] = new double[n];
+        PriorityQueue<pair> pq = new PriorityQueue<>((a,b)->{
+            if(b.getValue()>a.getValue()){
+                return 1;
+            }
+            return -1;
+
+        });
         Arrays.fill(prob,0);
-  PriorityQueue<pair>pq=new PriorityQueue<>((a,b)-> (int) (b.value-a.value));
-  pq.add(new pair(start,1));
-        prob[0]=1;
-while(!pq.isEmpty()){
-    pair p=pq.poll();
-    int u=p.key;
-   // double cost=p.value;
-    for(pair v:adj.get(u)){
-        if(prob[v.key]==1){
-            prob[v.key]=prob[u]*v.value;
-            pq.add(new pair(v.key,prob[v.key]));
-        }else{
-            if(prob[v.key]<prob[u]*v.value){
-                prob[v.key]=prob[u]*v.value;
-                pq.add(new pair(v.key,prob[v.key]));
+        prob[start]=1;
+        pq.offer(new pair(start,1));
+
+        while(!pq.isEmpty()){
+            int u = pq.poll().getKey();
+
+            for(pair p :adj.get(u)){
+                int v = p.getKey();
+                double cost = p.getValue();
+
+                if(prob[v]==0){
+                    prob[v] = prob[u]*cost;
+                    pq.offer(new pair(v,prob[v]));
+                }else{
+                    if(prob[u]*cost>prob[v]){
+                        prob[v] = prob[u]*cost;
+                        pq.offer(new pair(v,prob[v]));
+                    }
+                }
             }
         }
-    }
-}
+
         return prob[end];
     }
 }
