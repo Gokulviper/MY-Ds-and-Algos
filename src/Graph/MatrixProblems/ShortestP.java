55c11a9161c328da28054ea77d3c5db0427f3ea5
Gokulviper <deangokul57@gmail.com>
2022-10-02 09:17:00 +0530

    updated

---
+++ src/Graph/MatrixProblems/ShortestP.java
@@ -0,0 +1,14 @@
+package Graph.MatrixProblems;
+
+public class ShortestP {
+    public static void main(String[] args) {
+        int[][]mat=new int[7][7];
+        int cr=2;
+        int cc=3;
+        int tr=5;
+        int tc=5;
+        mat[cr][cc]=1;
+        mat[tr][tc]=2;
+
+    }
+}
